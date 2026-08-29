cc32e5cdaa20ed7fe670d7b947fa49685904d731
Kishan M <30570910+KishanMevada@users.noreply.github.com>
2026-08-29 23:58:25 +0530

    29Aug2026 2357 feat: add detail screen and pass navigation arguments safely (#6)

--- app/src/main/java/com/repolenspro/ui/navigation/AppNavGraph.kt
+++ app/src/main/java/com/repolenspro/ui/navigation/AppNavGraph.kt
@@ -2,9 +2,11 @@ package com.repolenspro.ui.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.repolenspro.ui.detail.RepositoryDetailScreen
 import com.repolenspro.ui.search.SearchScreen
 
@@ -15,14 +17,24 @@ fun AppNavGraph() {
     NavHost(navController = navController, startDestination = "search") {
         composable("search") {
             SearchScreen(
-                onNavigationToDetail = {
-                    navController.navigate("detail")
+                onNavigationToDetail = { repoName ->
+                    if (repoName.isNotBlank()) {
+                        val encodedName = java.net.URLEncoder.encode(repoName, "UTF-8")
+                        navController.navigate("detail/$encodedName")
+                    }
                 }
             )
         }
 
-        composable("detail") {
+        composable(
+            route = "detail/{repoName}",
+            arguments = listOf(navArgument("repoName") { type = NavType.StringType })
+        ) { backStackEntry ->
+
+            val repoName = backStackEntry.arguments?.getString("repoName") ?: "Unknown"
+
             RepositoryDetailScreen(
+                repoName = repoName,
                 onBackClick = {
                     navController.popBackStack()
                 }
